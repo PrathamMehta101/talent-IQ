30b826d40228f12a4390deecfb9036d4ea04929d
PrathamMehta101 <pratham10mehta@gmail.com>
2026-06-17 14:31:56 +0530

    auth and db synched

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,18 @@
 import express from "express";
+import cors from "cors";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { serve } from "inngest/express";
+import { inngest } from "./lib/inngest.js";
 
 const app = express();
+
+// middlewares
+app.use(express.json());
+//credentials=true => server allows browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "success from api" });
